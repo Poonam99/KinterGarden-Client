7a6ca60f6942d4a82d05e263792dc0efe6f552b3
Poonam99 <poonamtabassum9909@gmail.com>
2022-10-27 21:13:50 +0600

    courses

--- src/Pages/Course/Course.js
+++ src/Pages/Course/Course.js
@@ -2,14 +2,26 @@ import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Pdf from "react-to-pdf";
+import { FaPrint } from 'react-icons/fa';
+
+
+const ref = React.createRef();
 
 const Course = () => {
     const course = useLoaderData()
     console.log(course)
     return (
         <div className=''>
+            <div className='d-flex align-items-end justify-content-end p-5 ' >
+                <Pdf targetRef={ref} filename='code-example.pdf'>
+                    {({ toPdf }) =>
+                        <button onClick={toPdf} className='h-8 w-8'><FaPrint className='h-8 w-8' /></button>
+                    }
+                </Pdf>
+            </div>
             <div className=''>
-                <Card >
+                <Card ref={ref} >
                     <Card.Img variant="top" className='w-sm-50' src={course.image} />
                     <Card.Body>
                         <Card.Title>{course.name}</Card.Title>
