d2a4f2d4749756ea37a20eecb3f11224c74719a1
Poonam99 <poonamtabassum9909@gmail.com>
2022-10-27 18:50:55 +0600

    course

---
+++ src/Pages/Course/Course.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+const Course = () => {
+    const course = useLoaderData()
+    console.log(course)
+    return (
+        <div className=''>
+            <div className=''>
+                <Card >
+                    <Card.Img variant="top" src={course.image} />
+                    <Card.Body>
+                        <Card.Title>{course.name}</Card.Title>
+                        <Card.Text>
+                            {course.description}
+                        </Card.Text>
+                        <Link to={`/course/${course.id}/checkout`}><Button variant="primary">Get Premium Access</Button></Link>
+                    </Card.Body>
+                </Card>
+            </div>
+        </div>
+    );
+};
+
+export default Course;
